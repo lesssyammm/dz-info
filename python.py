1bd4aa09aae7f43304e62d2d6bdc5339f9bd94aa
lesssyammm <olesya.m.makarenko@gmail.com>
2025-02-27 23:43:18 +0300

    табуляция

--- python.py
+++ python.py
@@ -3,8 +3,8 @@ from math import sqrt
 def f(n):
     k = 0
     for d in range(1, int(sqrt(n)) + 1):
-            if n % d == 0:
-                  k += 1
+      if n % d == 0:
+            k += 1
     if k == 1:
           return True
     else:
