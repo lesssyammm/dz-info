7b45c2e3fca6327e3600664267fc62b39a191922
lesssyammm <olesya.m.makarenko@gmail.com>
2025-02-27 23:35:32 +0300

    gh

---
+++ dz.py/srabota.py
@@ -0,0 +1,37 @@
+# #1
+
+# from random import randint
+# a = []
+# n = 8
+# for i in range(n):
+#     a.append(randint(1, 100))
+
+# print(a)
+
+# for i in range(n//2 - 1):
+#     for j in range(n//2 - 1 - i):
+#         if a[j] > a[j + 1]:
+#             a[j], a[j + 1] = a[j + 1], a[j]
+
+# for l in range(n//2, n):
+#     for k in range(n//2, n - 1):
+#         if a[k] < a[k + 1]:
+#             a[k], a[k + 1] = a[k + 1], a[k]
+
+# print(a)
+
+#2
+from random import randint
+
+a = []
+n = 8
+for i in range(n):
+    a.append(randint(1, 100))
+
+print(a)
+k1, k2 = 0, 0
+for i in range(n-1):
+    for j in range(-1, -n+i, -1):
+        if a[j] < a[j - 1]:
+            a[j], a[j - 1] = a[j - 1], a[j]
+print(a)
---
+++ fjnfjn.py
@@ -0,0 +1,15 @@
+#кол-во делителей
+from math import sqrt
+def f(n):
+    t = 0
+    d = 1
+    while d < sqrt(n):
+        if n % d == 0:
+            t += 1
+        d += 1
+    if sqrt(n) == int(sqrt(n)):
+        t += 1
+    return t
+n = int(input())
+x = f(n)
+print(x)
