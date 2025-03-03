7c78fea85f856071bf9f5634f609d2623e9409b2
lesssyammm <olesya.m.makarenko@gmail.com>
2025-03-03 17:10:42 +0300

    Сортировка вставками

---
+++ dz.py/sort.py
@@ -0,0 +1,15 @@
+#Сортировка вставками
+from random import randint
+n = 15
+a = [randint(1, 30) for i in range(n)]
+print(a)
+print(sorted(a))
+for i in range(1, n):
+    el = a[i]    
+    ind = i
+    while a[ind - 1] > el and ind - 1 >= 0:
+        a[ind] = a[ind - 1]        
+        ind = ind - 1    
+    a[ind] = el
+
+print(a)
