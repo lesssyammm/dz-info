3d65968c50ee53fc63b8705ec42413e6fabc7506
lesssyammm <olesya.m.makarenko@gmail.com>
2025-03-12 23:40:51 +0300

    Работа с файлом текст

--- dz.py/11.py
+++ dz.py/11.py
@@ -1,26 +1,26 @@
-# #11
+#11
 
-# path = "C:\\Users\\olesy\\Desktop\\Новая папка (2)\\"
-# with open(path + "11_1.txt", "rt") as f1, open(path + "11_2.txt", "wt") as f2:
-#     line = f1.readline()
-#     while line:
-#         b = bin(int(line))
-#         o = oct(int(line))
-#         h = hex(int(line))
-#         f2.write(b + " " + o + " " + h + "\n")
-#         line = f1.readline()
+path = "C:\\Users\\olesy\\Desktop\\Новая папка (2)\\"
+with open(path + "11_1.txt", "rt") as f1, open(path + "11_2.txt", "wt") as f2:
+    line = f1.readline()
+    while line:
+        b = bin(int(line))
+        o = oct(int(line))
+        h = hex(int(line))
+        f2.write(b + " " + o + " " + h + "\n")
+        line = f1.readline()
 
 # #14
 
-# path = "C:\\Users\\olesy\\Desktop\\Новая папка (2)\\"
-# f1 = open(path + "14.txt", "r")
-# s = f1.readlines()
-# s = [int(i) for i in s]
-# s.sort(key = lambda x: int(str(abs(x))[-1]))
-# f1.close()
-# f2 = open(path + "14_2.txt", "w")
-# f2.write(f'Числа по возрастанию последней цифры: {s}\n')
-# f2.close()
+path = "C:\\Users\\olesy\\Desktop\\Новая папка (2)\\"
+f1 = open(path + "14.txt", "r")
+s = f1.readlines()
+s = [int(i) for i in s]
+s.sort(key = lambda x: int(str(abs(x))[-1]))
+f1.close()
+f2 = open(path + "14_2.txt", "w")
+f2.write(f'Числа по возрастанию последней цифры: {s}\n')
+f2.close()
 
 #16
 
---
+++ dz.py/file.py
@@ -0,0 +1,71 @@
+#1
+
+# path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
+# f = open(path + "файл к задачам 19 и 21(1).txt", encoding="utf8")
+# s = f.readlines()
+# f.close()
+# for i in range(len(s)):
+#     s[i] = s[i].strip()
+# s = " ".join(s)
+# s = s.split()
+# n = 0
+# for j in s:
+#     if len(j) > n:
+#         n = len(j)
+#         st = j
+# print(st)
+# f.close()
+
+#2
+
+# path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
+# file = "файл к задачам 19 и 21(1).txt"
+# f1 = open(path + file, encoding="utf8")
+# s = f1.readlines()
+# for i in range(len(s)):
+#     s[i] = s[i].strip()
+    
+# s = " ".join(s).lower()
+# s = s.split()
+# f1.close()
+# f2 = open(path + "new.txt", "w")
+# a = []
+# for j in s:
+#     if j[0] == "И" or j[0] == "и":
+#         f2.write(j + "\n")
+#         a.append(j)
+
+# print(len(a)) #4195
+# f2.close()
+
+#3
+
+path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
+file = "файл к задачам 19 и 21(1).txt"
+f1 = open(path + file, encoding="utf8")
+s = f1.readlines()
+for i in range(len(s)):
+    s[i] = s[i].strip()
+    
+s = " ".join(s).lower()
+s = s.split()
+f1.close()
+
+f2 = open(path + "new2.txt", "w")
+a = []
+for x in s:
+    for j in range(len(x)):
+        if len(x) % 2 == 0:
+            if x[0:j//2] == x[len(x):j//2:-1]:
+                f2.write(x + "\n")
+                if x not in a:
+                    a.append(x)
+        else:
+            if x[0:j//2 + 1] == x[len(x):j//2 + 1:-1]:
+                f2.write(x + "\n")
+                if x not in a:
+                    a.append(x)
+print(*a)
+print(len(a)) #как еще или тут тот iii летел еле оно ими обо иди и-и мадам ото уху xix комок
+#18
+f2.close()
