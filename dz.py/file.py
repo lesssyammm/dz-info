8fb6fd8955427ef2c15f0708a35cba02bf4ee4e4
lesssyammm <olesya.m.makarenko@gmail.com>
2025-03-12 23:43:43 +0300

    Работа с файлом текст

--- dz.py/file.py
+++ dz.py/file.py
@@ -1,42 +1,42 @@
 #1
 
-# path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
-# f = open(path + "файл к задачам 19 и 21(1).txt", encoding="utf8")
-# s = f.readlines()
-# f.close()
-# for i in range(len(s)):
-#     s[i] = s[i].strip()
-# s = " ".join(s)
-# s = s.split()
-# n = 0
-# for j in s:
-#     if len(j) > n:
-#         n = len(j)
-#         st = j
-# print(st)
-# f.close()
+path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
+f = open(path + "файл к задачам 19 и 21(1).txt", encoding="utf8")
+s = f.readlines()
+f.close()
+for i in range(len(s)):
+    s[i] = s[i].strip()
+s = " ".join(s)
+s = s.split()
+n = 0
+for j in s:
+    if len(j) > n:
+        n = len(j)
+        st = j
+print(st)
+f.close()
 
 #2
 
-# path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
-# file = "файл к задачам 19 и 21(1).txt"
-# f1 = open(path + file, encoding="utf8")
-# s = f1.readlines()
-# for i in range(len(s)):
-#     s[i] = s[i].strip()
+path = "C:\\Users\\olesy\\Desktop\\Файлы\\"
+file = "файл к задачам 19 и 21(1).txt"
+f1 = open(path + file, encoding="utf8")
+s = f1.readlines()
+for i in range(len(s)):
+    s[i] = s[i].strip()
     
-# s = " ".join(s).lower()
-# s = s.split()
-# f1.close()
-# f2 = open(path + "new.txt", "w")
-# a = []
-# for j in s:
-#     if j[0] == "И" or j[0] == "и":
-#         f2.write(j + "\n")
-#         a.append(j)
+s = " ".join(s).lower()
+s = s.split()
+f1.close()
+f2 = open(path + "new.txt", "w")
+a = []
+for j in s:
+    if j[0] == "И" or j[0] == "и":
+        f2.write(j + "\n")
+        a.append(j)
 
-# print(len(a)) #4195
-# f2.close()
+print(len(a)) #4195
+f2.close()
 
 #3
 
