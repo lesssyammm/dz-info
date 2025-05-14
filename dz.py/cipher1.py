85510b832dbcd1def3876f1fef1293b67ebc1d6b
lesssyammm <olesya.m.makarenko@gmail.com>
2025-05-14 23:11:52 +0300

    шифр Виженера

--- dz.py/cipher1.py
+++ dz.py/cipher1.py
@@ -1,41 +1,90 @@
+# a = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+# s = "ПРИШЕЛ УВИДЕЛ ПОБЕДИЛ"
+# k = "ЗАБЕГ"
+# r = ""
+# h = 0
+
+# for i in s:
+#     if i not in a:
+#         r += i
+#     else:
+#         i = a[(a.index(i) + a.index(k[h % len(k)])) % len(a)]
+#         r += i
+#         h += 1
+
+# print(r)
+
+
 #1
 
 a = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-s = "ЛЮДИ ОХОТНО ВЕРЯТ ТОМУ, ЧЕМУ ЖЕЛАЮТ ВЕРИТЬ"
-k = 6
-r = ""
+s = "ПРИШЕЛ УВИДЕЛ ПОБЕДИЛ"
+k = ""
+r = "ЩЫЖППЦ СЩТПГВ ЩЩЯЬОУЙ"
+h = 0
+
 for i in s:
     if i not in a:
-        r += i
+        k += i
     else:
-        r += a[(a.index(i) + k) % 32]
-        
-print(r)       
+        i = a[(len(a) - a.index(i) + a.index(r[h])) % len(a)]
+        k += i
+    h += 1
+
+print(k) #ключ
+
 
 #2
 
 a = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-s = input()
-k = int(input())
+s = "ПРИШЕЛ УВИДЕЛ ПОБЕДИЛ"
+k = ""
+r = "ЗШЬИУН УПРЙЭУ ГЮПЗДХУ"
+h = 0
+
 for i in s:
     if i not in a:
-        r += i
+        k += i
     else:
-        r += a[(a.index(i) + k) % 32]
+        i = a[(len(a) - a.index(i) + a.index(r[h])) % len(a)]
+        k += i
+    h += 1
 
-print(k)
+print(k) #шифрование
+
 
 #3
 
 a = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-s = "ХШЖНПУТ ФКХКОЙКТ"
+s = "ПРИШЕЛ УВИДЕЛ ПОБЕДИЛ"
+k = ""
+r = "ЧЭЬЖХЧ УФРОЕУ ЬВПХРИЭ"
+h = 0
 
-for k in range(1, 32):
-    r = ""
-    for i in s:
-        if i not in a:
-            r += i
-        else:
-            r += a[(a.index(i) - k) % 32]
-    print(k, r)
-# 5 РУБИКОН ПЕРЕЙДЕН
+for i in s:
+    if i not in a:
+        k += i
+    else:
+        i1 = a[(len(a) - a.index(i) + a.index(r[h])) % len(a)]
+        k += i1
+    h += 1
+
+print(k) #информатика
+
+#4
+
+a = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+s = ""
+k = "КРИПТОСТОЙКОСТЬ"
+r = "ЭБЪЭЫЕЩФЬЪЬК ЙЪРЪР Т ЯТЯЙЪВЩШРЫЧ ЭПЧ ПЬТЫЩС ШФИЕС"
+h = 0
+
+for i in r:
+    if i not in a:
+        s += i
+    else:
+        i1 = a[(len(a) - a.index(k[h % len(k)]) + a.index(i)) % len(a)]
+        s += i1
+        h += 1
+
+print(s) #УСТОЙЧИВОСТЬ ШИФРА К РАСШИФРОВКЕ БЕЗ ЗНАНИЯ КЛЮЧА
